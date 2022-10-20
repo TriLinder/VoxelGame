c28eb16dac6e826de1264757c4c5ae2e349f9500
TriLinder <82846064+TriLinder@users.noreply.github.com>
2022-10-20 16:37:03 +0200

    Implement block placing and block picking

--- player.py
+++ player.py
@@ -20,9 +20,13 @@ class Player :
         self.pitch = 0
 
         self.lookingAt = None
+        self.lookingAtEmptyBlock = None
         self.lastPunchTimestamp = -1
 
+        self.selectedBlockId = None
+
         self.cameraHeight = 2
+        self.reach = 8
 
     def getChunk(self) :
         x, y, z = self.position
@@ -34,23 +38,31 @@ class Player :
     
     def losBlock(self, maxRange) :
         forward = self.physics.forward
+        lastEmptyBlock = None
 
         for r in range(maxRange) :
-            pos = (self.position + glm.vec3(0, self.cameraHeight, 0)) + forward * r
+            pos = self.camera.position + forward * r
 
             chunk = self.scene.chunkObjectFromBlockCoords(pos.x, pos.z)
             if chunk :
                 block = chunk.getBlockFromAbsoulteCoords(pos)
 
                 if block and block.physicalBlock :
-                    return block
+                    self.lookingAt = block
+                    self.lookingAtEmptyBlock = lastEmptyBlock
 
-        return None
+                    return self.lookingAt
+                
+                lastEmptyBlock = block
+
+        self.lookingAt = None
+        self.lookingAtEmptyBlock = None
+        return self.lookingAt
     
     def blockBreaking(self) :
-        if pg.mouse.get_pressed()[0] : #LMB Pressed
-            self.lookingAt = self.losBlock(8)
-            
+        self.losBlock(self.reach)
+
+        if pg.mouse.get_pressed()[0] : #LMB Pressed, break block
             if self.lookingAt and time.time() - self.lastPunchTimestamp > 0.25 :
                 self.lastPunchTimestamp = time.time()
 
@@ -58,6 +70,19 @@ class Player :
                 block.changeId("air")
                 chunk = block.chunk
                 chunk.cullNeighbors(block.chunkRelativePos)
+        elif pg.mouse.get_pressed()[2] : #RMB Pressed, place block
+            if self.selectedBlockId and self.lookingAtEmptyBlock and time.time() - self.lastPunchTimestamp > 0.25 :
+                self.lastPunchTimestamp = time.time()
+
+                block = self.lookingAtEmptyBlock
+                block.changeId(self.selectedBlockId)
+                chunk = block.chunk
+                chunk.cullNeighbors(block.chunkRelativePos)
+        elif pg.mouse.get_pressed()[1] : #Middle mouse button pressed, pick block
+            if self.lookingAt :
+                block = self.lookingAt
+
+                self.selectedBlockId = block.id
         else :
             self.lastPunchTimestamp = -1
 
